0a388ba058ac7384f04c267c0bb9f3f33b73f5b0
GE08T <gaerthamgk@gmail.com>
2024-01-28 23:25:53 +0700

    chore(js-dom): add event probagation and how it's work

---
+++ Basic/js-dom/basic/src/event-probagation.js
@@ -0,0 +1,14 @@
+// event probagation adalah bagaimana cara dom bekerja pada javascript
+// ada 3 fase yaitu event capturing, target, event bubbling
+
+{/* <section class="event-probagation">
+  <h1 id="tittle">Halo</h1>
+  <div>
+    <button>click</button>
+  </div>
+</section> */}
+
+// jika kita ingin menambahkan event kedalam button maka javascript akan menulusi nya dari : window > document > html > body > section > div > button ini dinamakan event capturing
+// jika sudah di temukan maka akan menyeleksi target yang kemudian akan di kembalikan ulang / di telusuri ulang kembali ke window dengan event bubbling
+
+// ? method yang berguna dalam event probagation adalah stopPropagation dan preventDefault 
